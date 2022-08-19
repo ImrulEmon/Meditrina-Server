5329fd47afe072875cafe9475d8b1e60b17696a5
ImrulEmon <emimrulemon@gmail.com>
2022-08-19 23:34:30 +0600

    DB connected,/service get api

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.send("meditrina server running")
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(port,()=>{
-    console.log(`Listening to port ${port}`)
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzvvcsu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("medi_trina");
+    const serviceCollection = database.collection("services");
+
+    app.get('/service',async(req,res)=>{
+        const query = {};
+        const cursor=serviceCollection.find(query);
+        const services=await cursor.toArray();
+        res.send(services)
+
+
+
+    })
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("meditrina server running");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
