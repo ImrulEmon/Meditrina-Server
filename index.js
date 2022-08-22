88d089ec4316199857531460cc63d0471f79c65c
ImrulEmon <emimrulemon@gmail.com>
2022-08-22 19:52:54 +0600

    appointing one appointment at a single date

--- index.js
+++ index.js
@@ -24,20 +24,37 @@ async function run() {
     const serviceCollection = database.collection("services");
     const bookingCollection = database.collection("bookings");
     //GET API - READ DATA
-    app.get('/service',async(req,res)=>{
-        const query = {};
-        const cursor=serviceCollection.find(query);
-        const services=await cursor.toArray();
-        res.send(services)
-    })
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-     // POST API - ADD VOLUNTEER
-     app.post("/booking", async (req, res) => {
-        const booking = req.body;
-        const result = await bookingCollection.insertOne(booking);
-        res.json(result);
-      });
+    /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+     */
 
+    // POST API - ADD Bookings
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
   } finally {
   }
 }
